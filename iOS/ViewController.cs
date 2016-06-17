f780adea46fa381f7c8142eb105d865250e5e5d5
onion <onion.tsai@jetfusion.com.tw>
2016-06-17 14:37:50 +0800

    Add icon and iOS's map view layout

--- iOS/ViewController.cs
+++ iOS/ViewController.cs
@@ -14,14 +14,35 @@ namespace InterViewer.iOS
 		{
 			base.ViewDidLoad();
 
-			// Perform any additional setup after loading the view, typically from a nib.
+			Initial();
 
+			// Perform any additional setup after loading the view, typically from a nib.
 		}
 
 		public override void DidReceiveMemoryWarning()
 		{
 			base.DidReceiveMemoryWarning();
 			// Release any cached data, images, etc that aren't in use.		
+		}
+
+		protected void ChangeButtonState(object sender, EventArgs e)
+		{
+			var btn = sender as UIButton;
+			if (btn != null)
+			{
+				btn.Selected = !btn.Selected;
+			}
+		}
+
+		private void Initial()
+		{
+			btnNote.Enabled = false;
+			btnClock.Enabled = false;
+			btnTag.Enabled = false;
+			btnMicrophone.Enabled = false;
+			btnCamera.Enabled = false;
+
+			btnPencil.TouchUpInside += ChangeButtonState;
 		}
 	}
 }
--- iOS/ViewController.designer.cs
+++ iOS/ViewController.designer.cs
@@ -13,10 +13,58 @@ namespace InterViewer.iOS
 	partial class ViewController
 	{
 		[Outlet]
+		UIKit.UIButton btnCamera { get; set; }
+
+		[Outlet]
+		UIKit.UIButton btnClock { get; set; }
+
+		[Outlet]
+		UIKit.UIButton btnMicrophone { get; set; }
+
+		[Outlet]
+		UIKit.UIButton btnNote { get; set; }
+
+		[Outlet]
+		UIKit.UIButton btnPencil { get; set; }
+
+		[Outlet]
+		UIKit.UIButton btnTag { get; set; }
+
+		[Outlet]
 		MapKit.MKMapView map { get; set; }
 		
 		void ReleaseDesignerOutlets ()
 		{
+			if (btnCamera != null) {
+				btnCamera.Dispose ();
+				btnCamera = null;
+			}
+
+			if (btnClock != null) {
+				btnClock.Dispose ();
+				btnClock = null;
+			}
+
+			if (btnMicrophone != null) {
+				btnMicrophone.Dispose ();
+				btnMicrophone = null;
+			}
+
+			if (btnNote != null) {
+				btnNote.Dispose ();
+				btnNote = null;
+			}
+
+			if (btnPencil != null) {
+				btnPencil.Dispose ();
+				btnPencil = null;
+			}
+
+			if (btnTag != null) {
+				btnTag.Dispose ();
+				btnTag = null;
+			}
+
 			if (map != null) {
 				map.Dispose ();
 				map = null;
