88035f754cbb32928967a8a44c0f433ed6d1eb9a
onion <onion.tsai@jetfusion.com.tw>
2016-06-20 17:22:43 +0800

    Set android's detail view layout

---
+++ Droid/DetailActivity.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Debug = System.Diagnostics.Debug;
+namespace InterViewer.Droid
+{
+	[Activity(Label = "DetailActivity", ScreenOrientation = Android.Content.PM.ScreenOrientation.Landscape)]
+	public class DetailActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			SetContentView(Resource.Layout.Detail);
+		}
+	}
+}
+
