13e63fea33a4f44e4ec5d670f5a12fe6daa17cee
onion <onion.tsai@jetfusion.com.tw>
2016-06-21 17:06:15 +0800

    Add pdf template class and IService interface

---
+++ InterViewer/IService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterViewer
+{
+	public interface IService
+	{
+		List<PdfTemplate> GetPdfTemplates();
+
+		List<Document> GetDocuments();
+
+		void SaveAsJson(Document entity);
+	}
+}
+
---
+++ InterViewer/PdfTemplate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace InterViewer
+{
+	public class PdfTemplate
+	{
+		public string Name { get; set; }
+
+		public string Path { get; set; }
+	}
+}
+
