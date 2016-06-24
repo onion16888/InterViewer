c772658a6f6384a9b894c02ff6279820e50130e0
MaSean <Sean.Ma@jetfusion.com.tw>
2016-06-24 16:53:53 +0800

    Fix Return Button in FileManager

--- iOS/FileManagerController.cs
+++ iOS/FileManagerController.cs
@@ -13,7 +13,7 @@ namespace InterViewer.iOS
 	{
 		FileManagerTableSource source;
 		List<FileListAttributes> listFilePathName;
-		FileListAttributes pathRightNow;
+		String pathRightNow;
 		public FileManagerController (IntPtr handle) : base (handle)
 		{
 		}
@@ -22,11 +22,12 @@ namespace InterViewer.iOS
 			base.ViewDidLoad();
 
 			//Manager Start
-			listFilePathName = getFileAndPathList();
+			listFilePathName = getFileAndPathList("/");
+			//pathRightNow = "/";
 			//警告,FileManagerTableSource,可能造成記憶體問題
 			source = new FileManagerTableSource(listFilePathName);
 			FileManagerTableView.Source = source;
-			source.ItemClick += ListButtonClick;
+			source.ItemClick += ListButton_Click;
 			FileManagerTableView.ReloadData();
 
 			ReturnButton.TouchUpInside += ReturnButton_Click;
@@ -36,18 +37,20 @@ namespace InterViewer.iOS
 			base.DidReceiveMemoryWarning();
 			// Release any cached data, images, etc that aren't in use.
 		}
+		/// <summary>
+		/// Returns to the previou page
+		/// </summary>
 		private void ReturnButton_Click(object sender, EventArgs e)
 		{
 			char[] seperater = { '/' };
-			string[] pathTemp = pathRightNow.Name.Split(seperater);
+			string[] pathTemp = pathRightNow.Split(seperater);
 			string pathForQuery = string.Empty;
 			if (pathTemp.Length > 2)
 			{
 				for (int i = 1; i < pathTemp.Length - 1; i++)
 				{
+					pathForQuery += "/";
 					pathForQuery += pathTemp[i];
-					if (i != pathTemp.Length - 2)
-						pathForQuery += "/";
 				}
 			}
 			else
@@ -57,9 +60,10 @@ namespace InterViewer.iOS
 			listFilePathName = getFileAndPathList(pathForQuery);
 			source = new FileManagerTableSource(listFilePathName);
 			FileManagerTableView.Source = source;
-			source.ItemClick += ListButtonClick;
+			source.ItemClick += ListButton_Click;
 			FileManagerTableView.ReloadData();
 			ReturnButton.SetTitle("<< " + pathForQuery, UIControlState.Normal);
+			pathRightNow = pathForQuery;
 		}
 		/// <summary>
 		/// The list button clicked
@@ -67,48 +71,28 @@ namespace InterViewer.iOS
 		/// <returns>The button click.</returns>
 		/// <param name="sender">Sender.</param>
 		/// <param name="e">E.</param>
-		private void ListButtonClick(object sender, FileManagerTableSource.SelectedEventArgs e)
+		private void ListButton_Click(object sender, FileManagerTableSource.SelectedEventArgs e)
 		{
 			if (e.SelectedName.IsFile == true)
 			{
 				//Debug.WriteLine(e.SelectedName.Name);
-				File.Copy(e.SelectedName.Name, Environment.GetFolderPath(Environment.SpecialFolder.Personal) + @"/IntervView/Slides");
+				if (!File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.Personal) + @"/IntervView/Slides"))
+					Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.Personal) + @"/IntervView/Slides");
+				//File.Copy(e.SelectedName.Name, Environment.GetFolderPath(Environment.SpecialFolder.Personal) + @"/IntervView/Slides");
 				showAlert("新增範本", e.SelectedName.Name + @" 新增成功!!", @"確定", this);
-				ReturnButton.SetTitle("<< "+e.SelectedName.Name, UIControlState.Normal);
-				pathRightNow = e.SelectedName;
+				ReturnButton.SetTitle("<< " + e.SelectedName.Name, UIControlState.Normal);
+				pathRightNow = e.SelectedName.Name;
 			}
 			else
 			{
 				listFilePathName = getFileAndPathList(e.SelectedName.Name);
 				source = new FileManagerTableSource(listFilePathName);
 				FileManagerTableView.Source = source;
-				source.ItemClick += ListButtonClick;
+				source.ItemClick += ListButton_Click;
 				FileManagerTableView.ReloadData();
 				ReturnButton.SetTitle("<< "+e.SelectedName.Name, UIControlState.Normal);
-				pathRightNow = e.SelectedName;
+				pathRightNow = e.SelectedName.Name;
 			}
-		}
-		/// <summary>
-		/// Gets the file and path list.
-		/// </summary>
-		/// <returns>FileListAttributes include file's or folder's name and path</returns>
-		private List<FileListAttributes> getFileAndPathList()
-		{
-			List<FileListAttributes> theList = new List<FileListAttributes>();
-			FileListAttributes theFileAttributes;
-			string[] fileList = getFileList();
-			string[] pathList = getPathList();
-			for (int i = 0; i < fileList.Length; i++)
-			{
-				theFileAttributes = new FileListAttributes(true, fileList[i]);
-				theList.Add(theFileAttributes);
-			}
-			for (int i = 0; i < pathList.Length; i++)
-			{
-				theFileAttributes = new FileListAttributes(false, pathList[i]);
-				theList.Add(theFileAttributes);
-			}
-			return theList;
 		}
 		/// <summary>
 		/// Gets the file and path list.
@@ -137,29 +121,20 @@ namespace InterViewer.iOS
 		/// Gets the file list.
 		/// </summary>
 		/// <returns>The file list.</returns>
-		private string[] getFileList()
-		{
-			var files = Directory.EnumerateFiles("/");
-			return files.ToArray<String>();
-		}
-		/// <summary>
-		/// Gets the path list.
-		/// </summary>
-		/// <returns>The path list.</returns>
-		private string[] getPathList()
-		{
-			var paths = Directory.EnumerateDirectories("/");
-			return paths.ToArray<String>();
-		}
-		/// <summary>
-		/// Gets the file list.
-		/// </summary>
-		/// <returns>The file list.</returns>
 		/// <param name="path">The searching location.Example : "/",is the root place</param>
 		private string[] getFileList(string path)
 		{
-			var files = Directory.EnumerateFiles(path);
-			return files.ToArray<String>();
+			try
+			{
+				var files = Directory.EnumerateFiles(path);
+				return files.ToArray<String>();
+			}
+			catch (System.UnauthorizedAccessException)
+			{
+				showAlert("Permission denied", "無權進入此檔案夾", "確認", this);
+				var files = Directory.EnumerateFiles("/");
+				return files.ToArray<String>();
+			}
 		}
 		/// <summary>
 		/// Gets the path list.
@@ -168,8 +143,17 @@ namespace InterViewer.iOS
 		/// <param name="path">The searching location.Example : "/",is the root place</param>
 		private string[] getPathList(string path)
 		{
-			var paths = Directory.EnumerateDirectories(path);
-			return paths.ToArray<String>();
+			try
+			{
+				var paths = Directory.EnumerateDirectories(path);
+				return paths.ToArray<String>();
+			}
+			catch (System.UnauthorizedAccessException)
+			{
+				showAlert("Permission denied", "無權進入此檔案夾", "確認", this);
+				var paths = Directory.EnumerateDirectories("/");
+				return paths.ToArray<String>();
+			}
 		}
 		/// <summary>
 		/// Provide a simple way to show a alert window
@@ -184,18 +168,6 @@ namespace InterViewer.iOS
 			UIAlertController alertController = UIAlertController.Create(title, message, UIAlertControllerStyle.Alert);
 			alertController.AddAction(UIAlertAction.Create(actionTitle, UIAlertActionStyle.Default, null));
 			caller.PresentViewController(alertController, true, null);
-		}
-		/// <summary>
-		/// Provide a simple way to show a alert window
-		/// </summary>
-		/// <returns>The alert.</returns>
-		/// <param name="title">Title of the alert window</param>
-		/// <param name="message">Message</param>
-		/// <param name="actionTitle">The text on the check button</param>
-		/// <param name="caller">Base ViewController</param>
-		private void showAlert(string title, string message, string actionTitleOK, string actionTitleCancel, UIViewController caller)
-		{
-			//UIAlertController alertController = AlertViewControllerHelper.PresentOKCancelAlert(title, message, caller,
 		}
 	}
 }
