4d9dd5b7fb7e8e936cf7d255cf2d5e1a0ad6442f
onion <onion.tsai@jetfusion.com.tw>
2016-06-24 11:41:32 +0800

    iOS專案由其他app載入PDF檔後，自動進行預覽

---
+++ iOS/PDFUtils.cs
@@ -0,0 +1,65 @@
+﻿
+using System;
+using System.IO;
+using Foundation;
+using QuickLook;
+using UIKit;
+
+namespace InterViewer.iOS
+{
+	public class PDFItem : QLPreviewItem
+	{
+		string title;
+		string uri;
+
+		public PDFItem(string title, string uri)
+		{
+			this.title = title;
+			this.uri = uri;
+		}
+
+		public override string ItemTitle
+		{
+			get { return title; }
+		}
+
+		public override NSUrl ItemUrl
+		{
+			get { return NSUrl.FromFilename(uri); }
+		}
+	}
+
+	public class PDFPreviewControllerDataSource : QLPreviewControllerDataSource
+	{
+		string url = "";
+		string filename = "";
+
+		public PDFPreviewControllerDataSource(string url, string filename)
+		{
+			this.url = url;
+			this.filename = filename;
+		}
+
+		public override IQLPreviewItem GetPreviewItem(QLPreviewController controller, nint index)
+		{
+			return new PDFItem(filename, url);
+		}
+
+		public override nint PreviewItemCount(QLPreviewController controller)
+		{
+			return 1;
+		}
+	}
+
+	public class PdfUtils
+	{
+		public static void DisplayPdf(string path, UINavigationController parentController)
+		{
+			QLPreviewController previewController = new QLPreviewController();
+			previewController.DataSource = new PDFPreviewControllerDataSource(path, Path.GetFileName(path));
+
+			parentController.PushViewController(previewController, true);
+		}
+	}
+}
+
