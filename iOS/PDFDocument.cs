dd4b7f9e10e6c9deb51a0f1b1aec9ba591df6cd2
ＭichaelMac <michaeltainan@gmail.com>
2016-06-24 14:17:49 +0800

    修改PDFDocument可以只產生特定單張的圖擋

--- iOS/PDFDocument.cs
+++ iOS/PDFDocument.cs
@@ -22,7 +22,14 @@ namespace InterViewer.iOS
 			ConvertToImages();
 		}
 
-		private List<UIImage> ConvertToImages()
+		public PDFDocument(String FileName, int PageIndex)
+		{
+			Doc = CGPDFDocument.FromFile(FileName);
+			Count = (Int32)Doc.Pages;
+			ConvertToImages(PageIndex);
+		}
+
+		private List<UIImage> ConvertToImages( )
 		{
 			Images = new List<UIImage>();
 
@@ -85,6 +92,69 @@ namespace InterViewer.iOS
 					UIGraphics.EndImageContext();
 				}
 			}
+			return Images;
+		}
+
+		private List<UIImage> ConvertToImages(int i)
+		{
+			Images = new List<UIImage>();
+
+			if (Doc == null)
+			{
+				throw new Exception("Could not load document");
+			}
+
+				using (CGPDFPage Page = Doc.GetPage(i))
+				{
+					CGRect PageRect = Page.GetBoxRect(CGPDFBox.Media);
+					//nfloat Scale = 1;//View.Frame.Height / PageRect.Height;
+					//PageRect.Size = new CGSize(PageRect.Height * Scale, PageRect.Width * Scale);
+
+					if (PageRect.Height > PageRect.Width)
+					{
+						PageRect.Size = new CGSize(PageRect.Height, PageRect.Width);
+					}
+
+					CGRect MediaBox = Page.GetBoxRect(CGPDFBox.Media);
+					CGRect CropBox = Page.GetBoxRect(CGPDFBox.Crop);
+
+					nfloat TopMargin = CropBox.GetMinY() - MediaBox.GetMinY();
+					nfloat BottomMargin = MediaBox.GetMaxY() - CropBox.GetMaxY();
+					nfloat LeftMargin = CropBox.GetMinX() - MediaBox.GetMinX();
+					nfloat RightMargin = MediaBox.GetMaxX() - CropBox.GetMaxX();
+
+					if (TopMargin + BottomMargin + LeftMargin + RightMargin > 0)
+					{
+						PageRect = new CGRect(
+							PageRect.Location,
+							new CGSize(
+								PageRect.Size.Width - (LeftMargin + RightMargin),
+								PageRect.Size.Height - (TopMargin + BottomMargin)
+							)
+						);
+					}
+
+					UIGraphics.BeginImageContext(PageRect.Size);
+
+					using (CGContext context = UIGraphics.GetCurrentContext())
+					{
+						context.SaveState();
+
+						context.TranslateCTM(0, PageRect.Size.Height);
+						context.ScaleCTM(1, -1);
+
+						context.ConcatCTM(
+							Page.GetDrawingTransform(CGPDFBox.Crop, PageRect, 0, true)
+						);
+
+						context.DrawPDFPage(Page);
+						context.RestoreState();
+
+						Images.Add(UIGraphics.GetImageFromCurrentImageContext());
+					}
+
+					UIGraphics.EndImageContext();
+				}
 			return Images;
 		}
 
